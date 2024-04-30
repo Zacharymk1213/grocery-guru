502426a3c8d0e2f3e9e29844c389563d2ef58846
Jiafeng Lin <jiafeng.lin95@qmail.cuny.edu>
2024-04-30 04:55:33 -0400

    implemented searchItemsByName() in GroceryDatabase

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryDatabase.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryDatabase.java
@@ -1,5 +1,6 @@
 package edu.qc.seclass.glm;
 
+import java.util.TreeMap;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
@@ -44,7 +45,98 @@ public class GroceryDatabase {
      * @return an array of grocery items with similar name
      */
     public GroceryItem[] searchItemsByName(String name) {
-        return null; //placeholder
+        //find items of similar name
+        TreeMap<Double, GroceryItem> tem = new TreeMap<Double, GroceryItem>();
+        Set<Integer> itemIDs = db.keySet();
+        for (int id : itemIDs) {
+            GroceryItem thisItem = db.get(id);
+            double s = similarity(thisItem.getName(), name)
+            if (s > 0.4) //considerably similar
+                tem.put(s, thisItem);
+        }
+        //transfer top 10 from temporary to result array
+        GroceryItem[] result = new GroceryItem[10];
+        int i = 0;
+        GroceryItem item = tem.remove(tem.lastKey()); //get the most similar
+        while (i < 10 && item != null) {
+            result[i++] = item;
+            item = tem.remove(tem.lastKey()); //get the next
+        }
+        return result;
+    }
+    /**
+     * Returns a double between 0 and 1, 0 means not very similar in length and content
+     * and 1 means very similar in length and content <p>
+     * @param a
+     * @param b
+     * @return
+     */
+    private double similarity(String a, String b) {
+        return similarity(a, b, 3); //default to 3 characters
+    }
+    /**
+     * Returns a double between 0 and 1, 0 means not very similar in length and content
+     * and 1 means very similar in length and content <p>
+     * Overlap determines how much to favor consecutive blocks
+     * @param a
+     * @param b
+     * @param overlap
+     * @return
+     */
+    private double similarity(String a, String b, int overlap) {
+        //find longer and shorter
+        String longer = a, shorter = b;
+        if (a.length() < b.length()) {
+            shorter = a;
+            longer = b;
+        }
+        if (longer.length() == 0)
+            return 1.0;
+        shorter = shorter.toLowerCase();
+        longer = longer.toLowerCase();
+        //calculate rough perfect score (i.e. all consecutive match)
+        int perfect = 0;
+        {
+            int l = shorter.length();
+            for (int i = 1; i <= shorter.length() && i < overlap; i++) {
+                perfect += i;
+                l--;
+            }
+            perfect += l*overlap;
+        }
+        //find actual score
+        int score = 0, consec = 0, graced = 0, start;
+        for (int l = 0; l < longer.length(); l++) {
+            start = l;
+            for (int s = 0; s < shorter.length() && start+consec < longer.length(); s++) {
+                if (longer.charAt(start+consec) == shorter.charAt(s)) { //is a hit,
+                    if (consec < overlap) //increase next consecutive score
+                        consec++;
+                    else
+                        start++;
+                    if (consec > overlap/2) //probably not a coincidence
+                        l = start+consec; //move l along
+                    score += consec;
+                    //reduce graced
+                    if (graced > 0)
+                        graced--;
+                }
+                else if (consec > 0) { //failed, reset!
+                    start = l;
+                    consec = 0;
+                }
+            }
+            if (graced < overlap/2)
+                graced++;
+            else
+                score--;
+        }
+        double p = score/(double)perfect;
+        if (p > 1.0)
+            p = 1.0;
+        else if (p < 0.0)
+            p = 0.0;
+        return p;
     }
 
     /**
