9cf63a62bb5c5bcbdccef95b10f8c0334e2d6808
Jiafeng Lin <jiafeng.lin95@qmail.cuny.edu>
2024-05-02 17:55:39 -0400

    added adapter classes

---
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryItemAdapter.java
@@ -0,0 +1,17 @@
+package edu.qc.seclass.glm;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroceryItemAdapter extends ArrayAdapter {
+    
+}
