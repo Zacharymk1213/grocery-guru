14a7d183cbe3b3fbfff0067a39ecac77dce9aa35
Jiafeng Lin <jiafeng.lin95@qmail.cuny.edu>
2024-05-03 00:32:36 -0400

    Implemented Adapter

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryItemAdapter.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryItemAdapter.java
@@ -1,17 +1,57 @@
 package edu.qc.seclass.glm;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull; 
+import androidx.annotation.Nullable; 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.CheckBox;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class GroceryItemAdapter extends ArrayAdapter {
+    
+    public GroceryItemAdapter(@NonNull Context context, ArrayList<GroceryItem> arrayList) {
+        super(context, 0, arrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
+        View rowView = convertView;
+
+        if (rowView == null)
+            rowView = LayoutInflater.from(getContext()).inflate(
+                R.layout.list_item_layout, parent, false);
+            
+        GroceryItem thisItem = (GroceryItem)getItem(position);
+
+        TextView tvName = rowView.findViewById(R.id.tv_grocery_item_name);
+        tvName.setText(thisItem.getName());
+        TextView tvType = rowView.findViewById(R.id.tv_grocery_item_type);
+        tvType.setText(thisItem.getType());
+        TextView tvId = rowView.findViewById(R.id.tv_grocery_item_id); //here for testing purposes
+        tvId.setText(""+thisItem.getId());
+        CheckBox cbSelected = rowView.findViewById(R.id.grocery_item_checkbox);
+        if (thisItem.isSelected())
+            cbSelected.setChecked(true);
+        else
+            cbSelected.setChecked(false);
+
+        cbSelected.setOnCheckedChangeListener(new View.OnClickListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    thisItem.setSelected(true);
+                else
+                    thisItem.setSelected(false);
+            }
+        });
+
+        return rowView;
+    }
     
 }
--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryListAdapter.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GroceryListAdapter.java
@@ -1,17 +1,54 @@
 package edu.qc.seclass.glm;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull; 
+import androidx.annotation.Nullable; 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.CheckBox;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class GroceryListAdapter extends ArrayAdapter {
     
+    public GroceryListAdapter(@NonNull Context context, ArrayList<GroceryList> arrayList) {
+        super(context, 0, arrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
+        View rowView = convertView;
+
+        if (rowView == null)
+            rowView = LayoutInflater.from(getContext()).inflate(
+                R.layout.grocery_list_view, parent, false);
+            
+        GroceryList thisList = (GroceryList)getItem(position);
+
+        TextView tvName = rowView.findViewById(R.id.tv_grocery_list_name);
+        tvName.setText(thisList.getName());
+        TextView tvId = rowView.findViewById(R.id.tv_grocery_list_id); //here for testing purposes
+        tvId.setText(""+thisList.getId());
+        CheckBox cbSelected = rowView.findViewById(R.id.btn_grocery_list_selectable);
+        if (thisList.isSelected())
+            cbSelected.setChecked(true);
+        else
+            cbSelected.setChecked(false);
+
+        cbSelected.setOnCheckedChangeListener(new View.OnClickListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    thisList.setSelected(true);
+                else
+                    thisList.setSelected(false);
+            }
+        });
+
+        return rowView;
+    }
 }
