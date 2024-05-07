97a4beea5e51eecd15270e768c54722501c25fbf
Jiafeng Lin <jiafeng.lin95@qmail.cuny.edu>
2024-05-07 12:34:49 -0400

    added readManualText() to read manual

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/AppManualActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/AppManualActivity.java
@@ -1,7 +1,15 @@
 package edu.qc.seclass.glm;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 import java.util.ArrayList;
 
+import android.content.res.AssetManager;
+
+import android.util.Log;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +21,8 @@ import android.widget.TextView;
 import android.widget.ScrollView;
 
 public class AppManualActivity extends AppCompatActivity {
+
+    private String manualString;
 
     //GUI components
     Button btnBack;
@@ -28,6 +38,9 @@ public class AppManualActivity extends AppCompatActivity {
         tvManual = findViewById(R.id.tv_manual_body);
         svManualScroller = findViewById(R.id.sv_manual_scroller);
 
+        if (readManualText() == 0)
+            tvManual.setText(manualString);
+
         // Set click listener for the back button
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +50,31 @@ public class AppManualActivity extends AppCompatActivity {
             }
         });
     }
-    
+
+    /**
+     * Reads the file, simple_app_manual.txt, and loads it into manualString
+     * @return 0 if successfully
+     */
+    private int readManualText() {
+        try {
+            AssetManager am = getApplicationContext().getAssets();
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(am.open("simple_app_manual.txt"))
+            );
+            StringBuilder manualText = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null)
+                manualText.append(line + "\n");
+            manualString = manualText.toString();
+
+            // Close the streams
+            reader.close();
+
+            return 0; // Success
+        } catch (IOException e) {
+            // Handle IO exception
+            Log.e("readManualText", "Error reading manual text file: " + e.getMessage());
+        }
+        return -1; // Error
+    }
 }
